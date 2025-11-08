6e50441cdf5463b7446b93e392456270caeb086e
Apheksha B <apheksha.b@gmail.com>
2025-11-08 22:06:49 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,23 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
+import connectDB from "./lib/db.js";
 
-import  connectDB  from "./lib/db.js";
 const app = express();
 const __dirname = path.resolve();
 
-
-connectDB();
+await connectDB();
 
 app.use(
   cors({
-    origin: ["https://streamify-video-calls-frontend.vercel.app", "http://localhost:5173"],
+    origin: [
+      "https://streamify-video-calls-frontend.vercel.app",
+      "http://localhost:5173",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -29,9 +32,11 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/chat", chatRoutes);
 
-app.get("/", (req, res) => {
-  res.send(" Streamify backend is running on Vercel!");
-});
 app.get("/favicon.ico", (req, res) => res.status(204).end());
 
-export default app; 
+
+app.get("/", (req, res) => {
+  res.send("Streamify backend is running on Vercel!");
+});
+
+export default app;
